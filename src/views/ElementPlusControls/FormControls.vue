df0905e121497ba9591a8303b05983ce3df53a2f
yangjun <yangjun@163.com>
2024-11-05 17:49:01 +0800

    开发element=plus模块表单验证控件模块

--- src/views/ElementPlusControls/FormControls.vue
+++ src/views/ElementPlusControls/FormControls.vue
@@ -1,13 +1,80 @@
 <template>
   <div>
-    我是表单控件子组件
+    <el-form ref="ruleFormRef" :model="form" :rules="rules" label-width="auto" style="max-width: 600px">
+      <el-form-item label="Activity name" prop="name">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Activity zone" prop="region">
+        <el-select v-model="form.region" placeholder="please select your zone">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Activity time" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker
+              v-model="form.date1"
+              type="date"
+              placeholder="Pick a date"
+              style="width: 100%"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="2" class="text-center">
+          <span class="text-gray-500">-</span>
+        </el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker
+              v-model="form.date2"
+              placeholder="Pick a time"
+              style="width: 100%"
+            />
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Instant delivery" prop="delivery">
+        <el-switch v-model="form.delivery" />
+      </el-form-item>
+      <el-form-item label="Activity type" prop="type">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox value="Online activities" name="type">
+            Online activities
+          </el-checkbox>
+          <el-checkbox value="Promotion activities" name="type">
+            Promotion activities
+          </el-checkbox>
+          <el-checkbox value="Offline activities" name="type">
+            Offline activities
+          </el-checkbox>
+          <el-checkbox value="Simple brand exposure" name="type">
+            Simple brand exposure
+          </el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="Resources" prop="resource">
+        <el-radio-group v-model="form.resource">
+          <el-radio value="Sponsor">Sponsor</el-radio>
+          <el-radio value="Venue">Venue</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="Activity form" prop="desc">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit(ruleFormRef)">submit</el-button>
+        <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, defineProps, onMounted, watch, onUnmounted, getCurrentInstance, defineEmits } from 'vue';
+import { ref, reactive, computed, defineProps, onMounted, watch, onUnmounted, getCurrentInstance, defineEmits } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter, useRoute } from 'vue-router';
+import { ElMessage } from 'element-plus'
 
 const router = useRouter();
 const route = useRoute();
@@ -20,7 +87,64 @@ const props = defineProps({
 // 得到当前实例
 const instance = getCurrentInstance();
 
+const ruleFormRef = ref()
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+// 定义表单验证规则
+const rules = reactive({
+  name: [
+    { required: true, message: 'Please input Activity name', trigger: 'blur' },
+  ],
+  region: [
+    { required: true, message: 'Please select Activity zone', trigger: 'change' },
+  ],
+  date1: [
+    { type: 'date', required: true, message: 'Please pick a date', trigger: 'change' },
+  ],
+  type: [
+    { type: 'array', required: true, message: 'Please select at least one activity type', trigger: 'change' },
+  ],
+  resource: [
+    { required: true, message: 'Please select Activity zone', trigger: 'change' },
+  ],
+  desc: [
+    { required: true, message: 'Please input Activity form', trigger: 'blur' },
+  ],
+})
 
+const onSubmit = async (formEl) => {
+  console.log('onSubmit! formEl', formEl)
+  if (!formEl) return
+  // 提交之前加上表单验证的判断
+  await formEl.validate((valid, fields) => {
+    console.log('[] => valid', valid)
+    if (valid) {
+      ElMessage({
+        message: '验证成功.',
+        type: 'success',
+      });
+      console.log('submit!')
+    } else {
+      ElMessage({
+        message: '验证失败.',
+        type: 'error',
+      });
+      console.log('error submit!', fields)
+    }
+  })
+}
+const resetForm = (formEl) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
 // 生命周期
 onMounted(() => {
     console.log('mounted');
